52bd58474a20fd74f240140f43242e1f2bb23397
Punit Katiyar <punit.kat@gmail.com>
2023-07-23 22:50:31 +0530

    Add files via upload

---
+++ 04.js-dataType/function.js
@@ -0,0 +1,24 @@
+//Function: Represents a reusable block of code that performs a specific task.
+//Functions can be defined using the function keyword or as arrow functions(() => { }).
+
+// type 1
+
+const app = function hello() { return "Hello" };
+console.log(app());
+
+// type 2 un-named function
+const app1 = function () { return "hello 1" };
+console.log(app1());
+
+
+// type 3 arraow function
+const app2 = () => "hello";
+console.log(app2());
+
+
+
+
+
+
+
+
---
+++ 04.js-dataType/object.js
@@ -0,0 +1,33 @@
+// Object: Represents a collection of key-value pairs and is used to store more complex data.
+// Objects can be created using the new Object() syntax or object literals { }.
+
+// type 1
+const obj = {
+
+    uid: 101,
+    uname: "Admin",
+    uemail: "admin@techunit.com",
+    year: 2023   
+}
+console.log(obj);
+
+// type 2
+const user = {};
+user.id = 1;
+user.name = "Punit";
+user.year = 2023;
+console.log(user);
+
+// type 3
+
+const card = new Object();
+card.id = 101;
+card.name = "Admin";
+card.year = 2023;
+console.log(card);
+
+
+
+
+
+
