b8567cc1d9027405b286eecb5172358afef5ccd1
Punit Katiyar <punit.kat@gmail.com>
2023-12-19 18:23:12 +0530

    Update

--- 04.js-dataType/01.number.js
+++ 04.js-dataType/01.number.js
@@ -25,5 +25,53 @@ console.log(typeof a+b);
 let data=new Number(1000);
 console.log(data);
 
+let num1=10; // integer
+let num2=10.50; // float
+console.log(num1);
+console.log(num2);
+console.log(typeof num1);
+console.log(typeof(num2));
+
+// add two number
+
+let a=10;
+let b=30.50;
+let res1=a+b;
+console.log(res1);
+
+
+let res2=a*b;
+console.log(res2);
+
+let res3=a-b;
+console.log(res3);
+// add to number pro
+console.log(a+b);
+
+
+// basic math
+console.log(100+200/2-20);
+
+// increment ++ and decrement
+
+let count=1;
+console.log(count);
+
+count--;
+
+console.log(count);
+
+count--;
+
+console.log(count);
+
+
+
+
+
+
+
+
+
 
 
