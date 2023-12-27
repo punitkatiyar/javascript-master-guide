2cec4ebcd8bd5ab3eba1f8e67bc17de5accae9a1
Punit Katiyar <punit.kat@gmail.com>
2023-12-27 18:24:47 +0530

    Complete

---
+++ 06.js-control/switch.js
@@ -0,0 +1,14 @@
+let num="one";
+switch(num){
+    case "one":
+        console.log(1);
+        break;
+    case 'two':
+        console.log(2);
+        break;
+    default:
+        console.log('No Case Found');
+}
+
+
+
